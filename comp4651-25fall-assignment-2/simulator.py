54f8c26befbbbc7b94639af9740a96ed848a1d06
cloudera <cloudera@quickstart.cloudera>
2025-10-27 07:51:38 -0700

    part 4

---
+++ comp4651-25fall-assignment-2/simulator.py
@@ -0,0 +1,24 @@
+n():
+    acc = 0
+    with open("guest_program.txt", "r") as f:
+
+        lines = f.readlines()
+        for line in lines:
+            adj_line = line.strip()
+            if "add" in adj_line:
+                ad= adj_line.split()
+                acc += int(ad[1])
+                print(f"[Guest] Executing: add {ad[1]}")
+            elif "print" in adj_line:
+                print("[Guest] Executing: print")
+                print(f"Accumulator value: {acc}")
+            elif "halt" in adj_line:
+                print("[VMM] Trapped privileged instruction 'halt'. Halting guest.")
+
+            elif "scan_disk" in adj_line:
+                print("[VMM] Trapped privileged instruction 'scan_disk', emulating...")
+
+f __name__ == "__main__":
+    main()
+
+
